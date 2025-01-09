87f88a94d4c934fdef2f748eed84f7db1db47f3f
Hans-userboot <100776056+Hans-userboot@users.noreply.github.com>
2025-01-09 09:30:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,6 @@
 if __name__ == "__main__":
     import uvloop
     from core.bot import Bot
-    uvloop.install()
-    Bot().run()
+    run(self.start(use_qr=use_qr, except_ids=except_ids))
+                run(idle())
+                run(self.stop())
